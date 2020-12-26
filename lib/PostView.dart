33e065c990a7423180007ef622766fdbc619d570
Ayush Munot <ayushmunot@Ayushs-MacBook-Pro.local>
2020-12-26 15:10:26 -0500

    Post View Screen added

---
+++ lib/PostView.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PostView extends StatefulWidget {
+  @override
+  PostViewState createState() => PostViewState();
+}
+
+class PostViewState extends State<PostView> {
+  
+}
